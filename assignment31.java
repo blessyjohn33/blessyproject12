b6211d419a7f0a3380611461a5f13e6fdd29b4e0
unknown <blessyjohn3893@gmail.com>
2025-02-07 15:43:53 +0530

    first commit

---
+++ assignment31.java
@@ -0,0 +1,11 @@
+package assignment3;
+
+public class assignment31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("+ ''''' +\n[|  o o | ]\n|    ^  |\n|  '-'  |\n+----- +");
+
+	}
+
+}
